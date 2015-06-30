7aae35edf8f742302d40cc57a7dac174358d1eb9
Juleswong <magicalbaka@gmail.com>
2015-06-30 09:09:55 -0700

    Revert "Project Files"
    This reverts commit a2cafce030329eb97684ec659c7ece1e983c8e37.

--- abctesting/info/static/info/javascripts/controllers/MainController.js
+++
@@ -1,9 +0,0 @@
-app.controller('MainController', ['$scope', 'logbook', function($scope, logbook) {
-	$scope.subtitle = 'Log Book';
-	$scope.sortType = 'lab_number';
-	$scope.sortReverse = true;
-	$scope.searchLog = '';
-	logbook.success(function(data) {
-		$scope.logBook = data;
-	});
-}]);
--- abctesting/info/static/info/javascripts/services/samplelog.js
+++
@@ -1,9 +0,0 @@
-app.factory('logbook', ['$http', function($http) {
-	return $http.get('http://127.0.0.1:8000/info/api/?format=json')
-		.success(function(data) {
-			return data;
-		})
-		.error(function(err) {
-			return err;
-		})
-}])
